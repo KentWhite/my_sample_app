7688844b9dc55c4a119d5098e93d928dbcd47be6
KentWhite <Kent.White@hotmail.com>
2011-06-29 14:13:26 -0500

    Done with user edit/update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org'
 gem 'rails', '3.0.8'
 gem 'sqlite3', '1.3.3'
 gem 'gravatar_image_tag', '1.0.0.pre2'
+gem 'will_paginate', '3.0.pre2'
 
 gem 'rake', '0.8.7'
 
@@ -10,6 +11,7 @@ gem 'rake', '0.8.7'
 group :development do
   gem 'rspec-rails', '2.6.1'
   gem 'annotate-models', '1.0.4'
+  gem 'faker', '0.3.1'
 end
 
 group :test do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -133,7 +133,29 @@ describe User do
         matching_user = User.authenticate(@attr[:email], @attr[:password])
         matching_user.should == @user
       end
+    end # describe authenticate method
+    
+    
+  end # of password encryption
+  
+  describe "admin attribute" do
+
+    before(:each) do
+      @user = User.create!(@attr)
     end
-  end
+
+    it "should respond to admin" do
+      @user.should respond_to(:admin)
+    end
+
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+
+    it "should be convertible to an admin" do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
+  end # of admin attribute
   
 end
