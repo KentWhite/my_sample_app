11ba3ff5a0cc8f80cfa90666e64c87387415dab8
KentWhite <Kent.White@hotmail.com>
2011-07-19 11:42:49 -0500

    Updated Mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 MySampleApp::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.sendgrid.net",
+  :port => 25,
+  :domain => "sendgrid.net",
+  :authentication => :login,
+  :user_name => "SeecaMailer@smtp.sendgrid.net",
+  :password => "s56cat78ch"
+}
